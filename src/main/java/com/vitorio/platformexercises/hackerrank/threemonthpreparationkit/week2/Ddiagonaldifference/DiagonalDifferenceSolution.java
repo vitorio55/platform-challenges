809d0c016816c3a57de5eabafdc7cf3beb1a6bc4
Vitorio Mincarone <vitorio.tfm@gmail.com>
2024-02-27 18:00:49 -0300

    Add Hackerrank -> Diagonal Difference challenge solution

---
+++ src/main/java/com/vitorio/platformexercises/hackerrank/threemonthpreparationkit/week2/Ddiagonaldifference/DiagonalDifferenceSolution.java
@@ -0,0 +1,55 @@
+package com.vitorio.platformexercises.hackerrank.threemonthpreparationkit.week2.Ddiagonaldifference;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class DiagonalDifferenceSolution {
+
+    public static String main(File input) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(input));
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        List<List<Integer>> arr = new ArrayList<>();
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        bufferedReader.close();
+        return String.valueOf(Result.diagonalDifference(arr));
+    }
+
+    class Result {
+
+        /*
+         * Complete the 'diagonalDifference' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int diagonalDifference(List<List<Integer>> arr) {
+            int rows = arr.size();
+            int topLeftBottomRightDiagonalSum = 0;
+            int topRightBottomLeftDiagonalSum = 0;
+
+            for (int i = 0; i < rows; i++) {
+                List<Integer> row = arr.get(i);
+                topLeftBottomRightDiagonalSum += row.get(i);
+                topRightBottomLeftDiagonalSum += row.get((rows - 1) - i);
+            }
+
+            return Math.abs(topLeftBottomRightDiagonalSum - topRightBottomLeftDiagonalSum);
+        }
+    }
+}
