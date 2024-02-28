4739299b532ed8ec234616a48c2472278909361d
Vitorio Mincarone <vitorio.tfm@gmail.com>
2024-02-28 09:07:15 -0300

    Add Hackerrank -> Counting Valleys challenge solution

---
+++ src/main/java/com/vitorio/platformexercises/hackerrank/threemonthpreparationkit/week2/Fcoutingvalleys/CountingValleysSolution.java
@@ -0,0 +1,42 @@
+package com.vitorio.platformexercises.hackerrank.threemonthpreparationkit.week2.Fcoutingvalleys;
+
+import java.io.*;
+
+public class CountingValleysSolution {
+
+    public static String main(File input) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(input))) {
+            int steps = Integer.parseInt(bufferedReader.readLine().trim());
+            String path = bufferedReader.readLine();
+            return String.valueOf(Result.countingValleys(steps, path));
+        }
+    }
+
+    class Result {
+
+        /*
+         * Complete the 'countingValleys' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER steps
+         *  2. STRING path
+         */
+
+        public static int countingValleys(int steps, String path) {
+            int valleys = 0;
+            int height = 0;
+            for (int i = 0; i < path.length(); i++) {
+                if (path.charAt(i) == 'U') {
+                    if (height == -1) {
+                        valleys++;
+                    }
+                    height++;
+                } else {
+                    height--;
+                }
+            }
+            return valleys;
+        }
+    }
+}
